0bffb03ad826bf800f9213a5247e1940d64065cc
Sourabh Bhat <heySourabh@gmail.com>
2023-03-18 08:26:35 +0100

    rename variables

--- src/in/spbhat/wallpaper/impl/RandomGradient.java
+++ src/in/spbhat/wallpaper/impl/RandomGradient.java
@@ -9,7 +9,7 @@ import java.util.Collections;
 import java.util.Random;
 
 public class RandomGradient implements WallpaperGenerator {
-    record Colors(Color[] collection) {
+    record Palette(Color[] colors) {
     }
 
     record Point(int x, int y) {
@@ -20,11 +20,11 @@ public class RandomGradient implements WallpaperGenerator {
         }
     }
 
-    Colors[] colorsArray = {
-            new Colors(new Color[]{Color.decode("0xbfb190"), Color.decode("0xfff9f3")}),
-            new Colors(new Color[]{Color.decode("0x3b8fa3"), Color.decode("0x563953")}),
-            new Colors(new Color[]{Color.decode("0xa3483b"), Color.decode("0x3d4f66")}),
-            new Colors(new Color[]{Color.decode("0x6f493f"), Color.decode("0x313e2f")}),
+    Palette[] palettes = {
+            new Palette(new Color[]{Color.decode("0xbfb190"), Color.decode("0xfff9f3")}),
+            new Palette(new Color[]{Color.decode("0x3b8fa3"), Color.decode("0x563953")}),
+            new Palette(new Color[]{Color.decode("0xa3483b"), Color.decode("0x3d4f66")}),
+            new Palette(new Color[]{Color.decode("0x6f493f"), Color.decode("0x313e2f")}),
     };
 
     @Override
@@ -40,17 +40,17 @@ public class RandomGradient implements WallpaperGenerator {
         Point lower_right = new Point(width, height);
         Point upper_left = new Point(0, 0);
         Point upper_right = new Point(width, 0);
-        Point[] corners = {
-                lower_left, lower_right, upper_left, upper_right
-        };
+        Point[] corners = {lower_left, lower_right, upper_left, upper_right};
         Collections.shuffle(Arrays.asList(corners));
         Point corner1 = corners[0];
         Point corner2 = corners[1];
+
         Random rnd = new Random();
-        Colors colors = colorsArray[rnd.nextInt(colorsArray.length)];
-        Collections.shuffle(Arrays.asList(colors.collection));
-        Color color1 = colors.collection[0];
-        Color color2 = colors.collection[1];
+        Palette palette = palettes[rnd.nextInt(palettes.length)];
+        Collections.shuffle(Arrays.asList(palette.colors));
+        Color color1 = palette.colors[0];
+        Color color2 = palette.colors[1];
+
         BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
         Graphics2D g = image.createGraphics();
         g.setPaint(new GradientPaint(corner1.x, corner1.y, color1, corner2.x, corner2.y, color2));
@@ -63,22 +63,24 @@ public class RandomGradient implements WallpaperGenerator {
         Point lower_right = new Point(width, height);
         Point upper_left = new Point(0, 0);
         Point upper_right = new Point(width, 0);
-        Point[] corners = {
-                lower_left, lower_right, upper_left, upper_right
-        };
+        Point[] corners = {lower_left, lower_right, upper_left, upper_right};
+
         Random rnd = new Random();
         Point center = new Point(rnd.nextInt(width), rnd.nextInt(height));
         double radius = 0.0;
         for (Point corner : corners) {
             radius = Math.max(radius, center.distance(corner));
         }
-        Colors colors = colorsArray[rnd.nextInt(colorsArray.length)];
-        Collections.shuffle(Arrays.asList(colors.collection));
-        Color color1 = colors.collection[0];
-        Color color2 = colors.collection[1];
+
+        Palette palette = palettes[rnd.nextInt(palettes.length)];
+        Collections.shuffle(Arrays.asList(palette.colors));
+        Color color1 = palette.colors[0];
+        Color color2 = palette.colors[1];
+
         BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
         Graphics2D g = image.createGraphics();
-        g.setPaint(new RadialGradientPaint((float) center.x, (float) center.y, (float) radius, new float[]{0, 1}, new Color[]{color1, color2}));
+        g.setPaint(new RadialGradientPaint((float) center.x, (float) center.y, (float) radius,
+                new float[]{0, 1}, new Color[]{color1, color2}));
         g.fillRect(0, 0, width, height);
         return image;
     }
